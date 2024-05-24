8181e5375a3da311ceb75d3c769b0fd4bbe7eb1b
DanielDeanGithub <github-new@inboxrefresh.com>
2024-05-24 18:10:51 +0100

    Update contactInfo variable

--- src/Contact.js
+++ src/Contact.js
@@ -19,20 +19,20 @@ function Contact() {
   );
 
   const contactInfo = (
-    
+    <ul>
+      <li>
+        client@example.com
+      </li>
+      <li>
+        555.555.5555
+      </li>
+    </ul>
   );
 
   return (
       <div id="authorization">
         <h1>{authorized ? "Contact" : "Enter the Password"}</h1>
-        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
+
       </div>
   );
 }
